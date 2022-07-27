e0b466efabcaf4689b5d651785ce75bb7dd31a0f
markhuna <markhuna@gmail.com>
2022-07-27 07:45:20 +0400

    u

--- luneta/Build/UnityLoader.js
+++ luneta/Build/UnityLoader.js
@@ -2041,7 +2041,7 @@ var UnityLoader = UnityLoader || {
         }
     }(),
     compatibilityCheck: function (e, t, r) {
-        UnityLoader.SystemInfo.hasWebGL ? UnityLoader.SystemInfo.mobile ? e.popup("Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.", [{
+        UnityLoader.SystemInfo.hasWebGL ? UnityLoader.SystemInfo.smobile ? e.popup("Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.", [{
             text: "OK",
             callback: t
         }]) : ["Edge", "Firefox", "Chrome", "Safari"].indexOf(UnityLoader.SystemInfo.browser) == -1 ? e.popup("Please note that your browser is not currently supported for this Unity WebGL content. Press OK if you wish to continue anyway.", [{
